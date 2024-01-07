52437e92fae34b8fd584f62868ffb58b2f61bca1
caspian7400 <caspian7400@gmail.com>
2024-01-07 13:09:54 +0530

    track component changes

--- client/src/components/Track.jsx
+++ client/src/components/Track.jsx
@@ -1,11 +1,12 @@
 import { Container } from 'react-bootstrap'
 import PropTypes from 'prop-types'
+import "../styles/spotify/styles.css"
 
 export default function Track({ track }) {
   return (
-    <Container fluid className='mb-4 d-flex'>
+    <Container fluid className='mb-4 d-flex track-container'>
       <img src={track.images[0].url} alt='img' style={{ height: '80px', width: '80px' }} />
-      <div className='px-3'>
+      <div className='px-3 d-flex-column'>
         <h1>{track.name}</h1>
         {track.artists.map(artist => (<b key={artist.id}>{artist.name} </b>))}
       </div>
