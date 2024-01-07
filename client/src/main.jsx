6ef4e18fdf5e9d56819c7a4412641d2ca311e4ea
caspian7400 <caspian7400@gmail.com>
2024-01-07 13:09:26 +0530

    main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,8 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import 'bootstrap/dist/css/bootstrap.css'
-// import Test from './Test'
+import App from './App'
+import Test from './test/Test'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
